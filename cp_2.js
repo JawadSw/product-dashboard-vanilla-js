7516d728f4f3c0a07f89b8a01b84851a939269a5
Jawad Sweilem <jsweilem@usf.edu>
2025-10-19 14:57:54 -0400

    Finished step 4, added async and try/catch function to retrieve the product data. Used handle error if the fetch fails.

--- cp_2.js
+++ cp_2.js
@@ -18,3 +18,21 @@ products.forEach(product => {
     console.error("An error occurred:", error.message);
 });
 }
+
+fetchProductsThen();
+
+//step 4
+async function fetchProductsAsync() {
+    try {
+        const res = await fetch('https://www.course-api.com/javascript-store-products')
+        ;
+        if(!res.ok) {
+            throw new Error(`Network error: ${res.status}`);
+        }
+        const data = await res.json();
+        displayProducts(data);
+    } catch (error) {
+        handleError(error);
+    }
+
+    }
